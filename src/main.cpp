985f5e0a4b8b7644f98fa266b5a8fb83d5f43703
Finlie Orton <forton@softwareinstitute.com>
2022-05-17 10:18:59 +0100

    Adding updated main code

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,7 @@ int main()
         MyPen.turn();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-        /*std::cout << "Do you want another turn? (t)urn, e(x)it . .\n";
-        decision = getch();
-        switch (decision)
-        {
-        case 't':
-            MyPen.turn();
-            break;
-        case 'x':
-            exit = true;
-            break;
 
-        default:
-            std::cout << "Go again .. \n";
-            break;
-        }*/
     } while (exit == false);
 
     return 0;
--- src/thePen.cpp
+++ src/thePen.cpp
@@ -2,18 +2,22 @@
 #include <iostream>
 #include <string>
 #include "../include/thePen.h"
-#include <chrono>
 #include <thread>
 
 thePen::thePen()
 {
-    radioactiveInfection = 0;
+    noInfected = 0;
     for (int i = 0; i < 5; i++)
     {
         std::shared_ptr<bunny> NewBunny = std::make_shared<bunny>();
         std::cout << "Bunny " << NewBunny->getName() << " was born!\n";
-        MyBunnyList.push_back(NewBunny);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        MyBunnyList.push_back(NewBunny);
+        if (NewBunny->infected == 1)
+        {
+            noInfected++;
+        }
+        thePen::display(NewBunny->age, NewBunny->getName(), NewBunny->getInfected(), NewBunny->getColour(), NewBunny->getSex());
     }
 };
 
@@ -27,22 +31,21 @@ void thePen::turn()
 {
     breadingFemale = 0;
     breadingMale = false;
+    int noInfectedCopy = noInfected;
 
     for (std::list<std::shared_ptr<bunny>>::iterator f = MyBunnyList.begin(); f != MyBunnyList.end(); ++f)
     {
         (*f)->SetAge();
-        thePen::display((*f)->age, (*f)->getName(), (*f)->getInfected(), (*f)->getColour(), (*f)->getSex());
-        if (radioactiveInfection > 0)
+        if (noInfectedCopy > 0) // Infecting from the newborns
         {
             if ((*f)->infected == 0)
             {
                 (*f)->turnInfected();
-                radioactiveInfection--;
-                true;
+                noInfectedCopy--;
+                noInfected++; // comment this out to reduce infections and the simulation will last longer.
             }
         }
-        // This function adds the bunnys to the breading list
-        if ((*f)->age >= breadingAge && (*f)->infected == 0)
+        if ((*f)->age >= breadingAge && (*f)->infected == 0) // Setting the amount of breeding females and male
         {
             if ((*f)->i_sex == 1)
             {
@@ -54,17 +57,59 @@ void thePen::turn()
                 breadingMale = true;
             }
         }
-        if (((*f)->age == MaxAge && (*f)->infected == 0) || ((*f)->age == MaxInfectedAge))
+
+        if (((*f)->age == MaxAge && (*f)->infected == 0) || ((*f)->age == MaxInfectedAge)) // Death of bunny or display
         {
             std::cout << "Bunny " << (*f)->getName() << " has died\n";
-            MyBunnyList.erase(f--);
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            if ((*f)->infected == true)
+            {
+                noInfected--;
+            }
+            MyBunnyList.erase(f--);
+        }
+        else
+        {
+            thePen::display((*f)->age, (*f)->getName(), (*f)->getInfected(), (*f)->getColour(), (*f)->getSex());
         }
     }
-    // This breads the bunnys at the end of the go
+
+    thePen::breed();
+    std::cout << "AMOUNT OF BUNNIES:  " << MyBunnyList.size() << " NUMBER BORN INFECTED: " << noInfected << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    thePen::cull();
+};
+
+void thePen::cull()
+{
+    if (MyBunnyList.size() > MaxPopulation)
+    {
+        std::cout << "before the buuny cull size :: " << MyBunnyList.size() << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        int removeNum = MyBunnyList.size() / 2;
+        for (int g = 0; g < removeNum; g++)
+        {
+            std::list<std::shared_ptr<bunny>>::iterator k = MyBunnyList.begin();
+            int randomNumber = rand() % MyBunnyList.size();
+            std::advance(k, randomNumber);
+            std::cout << "Bunny " << (*k)->getName() << " has died\n";
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            if ((*k)->infected == true)
+            {
+                noInfected--;
+            }
+            MyBunnyList.erase(k);
+        }
+        std::cout << "After the bunny cull, population size is: " << MyBunnyList.size() << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
+void thePen::breed()
+{
     if (breadingMale == true && breadingFemale > 0)
     {
-        radioactiveInfection = 0;
+        // noInfected = 0;
         for (int i = 0; i < breadingFemale; i++)
         {
             std::shared_ptr<bunny> NewBunny = std::make_shared<bunny>(furColour.at(i));
@@ -73,30 +118,9 @@ void thePen::turn()
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             if (NewBunny->infected == 1)
             {
-                radioactiveInfection++;
+                noInfected++;
             }
             thePen::display(NewBunny->age, NewBunny->getName(), NewBunny->getInfected(), NewBunny->getColour(), NewBunny->getSex());
         }
     }
-
-    std::cout << "AMOUNT OF BUNNIES:  " << MyBunnyList.size() << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-    if (MyBunnyList.size() > MaxPopulation)
-    {
-
-        std::cout << "before the buuny cull size :: " << MyBunnyList.size() << std::endl;
-        int remove = MyBunnyList.size() / 2;
-        for (int g; g < remove; g++)
-        {
-            std::list<std::shared_ptr<bunny>>::iterator k = MyBunnyList.begin();
-            int randomNumber = rand() % MyBunnyList.size();
-            std::advance(k, randomNumber);
-            std::cout << "Bunny " << (*k)->getName() << " has died\n";
-            MyBunnyList.erase(k);
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        }
-
-        std::cout << "After the bunny cull, population size is: " << MyBunnyList.size() << std::endl;
-    }
-};
+}
